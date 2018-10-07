eced5159577404d38b41a3de987cbc921fb3d706
wangyp <wangyp@gmail.com>
2018-10-08 00:11:47 +0800

    commit

--- wf.cpp
+++ wf.cpp
@@ -1,12 +1,14 @@
 #include<iostream>
-#include<string.h>
 #include<algorithm>
 #include <bits/stdc++.h>
-#include <fstream>
 #include <io.h>
-#include<cstdio>
 #include<cstdlib>
-#include<io.h>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <cstdio>
+#include <vector>
+#include <map>
 using namespace std;
 int mode;
 int torf=1;
@@ -20,6 +22,13 @@ bool cmp(word a,word b)
     if(a.cnt>b.cnt)
         return true;
     return false;
+}
+map<string, int> Word1;
+
+void fcounter();
+bool word_cmp(pair<string, int> p1, pair<string, int> p2)
+{
+    return p1.second > p2.second;
 }
 char str[50];
 char str1[50];
@@ -120,80 +129,85 @@ void counter(char *strtemp)
 }
 void fcounter()
 {
-    long long total=0;
-    long long num=0;
-    char out[50];
-    getcwd(out,sizeof(out));
-    strcat(out,"\\");
-    strcat(out,"out.txt");
-    while(~scanf("%s",str))
+    long long total = 0;
+    long long num = 0;
+    string fileName = "out.txt";
+    char str[50];
+    char str1[50];
+    string temp;
+    while (~scanf("%s",str))
     {
-        int len=strlen(str);
-        int j=0;
-        for(int i=0; i<len; i++)
+        int len = strlen(str);
+        int j = 0;
+        for (int i = 0; i < len; ++i)
         {
-            if(str[i]>='A'&&str[i]<='Z')
-            {
-                str[i]+=32;
-            }
-            if(str[i]>='a'&&str[i]<='z')
-            {
-                str1[j++]=str[i];
-            }
+            if (str[i] >= 'A' && str[i] <= 'Z')
+                str[i] += 32;
+            if (str[i] >= 'a' && str[i] <= 'z')
+                str1[j++] = str[i];
             else
             {
-                str1[j]='\0';
-                if(str1[0]=='\0')
+                //str1[j] == '0';
+                str1[j] = 0;
+                j = 0;
+                if (str1[0] == 0)
                     continue;
                 else
-                    total++;
-                bool flag=true;
-                for(int i=0; i<num; i++)
-                {
-                    if(strcmp(Word[i].W,str1)==0)
-                    {
-                        Word[i].cnt++;
-                        flag=false;
-                        break;
-                    }
-                }
-                if(flag)
-                {
-                    strcpy(Word[num].W,str1);
-                    Word[num].cnt=1;
-                    num++;
-                }
-                j=0;
+                    ++total;
+                // 这里用map
+                temp = str1;
+                if (Word1.count(temp) > 0)
+                    Word1[temp]++;
+                else
+                    Word1[temp] = 1;
+                // else if (Word.count(temp) == 0)
+                //     Word[temp] = 1;
+                // else
+                //     ;
             }
         }
-        str1[j]='\0';
-        if(str1[0]=='\0')
-        {
-            continue;
-        }
-        //else
-        //{
-        //    total++;
-        //}
 
-        bool flag=true;
-        for(int i=0; i<num; i++)
-        {
-            if(strcmp(Word[i].W,str1)==0)
-            {
-                Word[i].cnt++;
-                flag=false;
-                break;
-            }
-        }
-        if(flag)
-        {
-            strcpy(Word[num].W,str1);
-            Word[num].cnt=1;
-            num++;
-        }
+        str1[j] = 0;
+        if (str1[0] == 0)
+            continue;
+        temp = str1;
+        if (Word1.count(temp) > 0)
+            Word1[temp]++;
+        else
+            Word1[temp] = 1;
+
     }
 
+    map<string, int>::iterator p;
+    vector<pair<string, int> > word_vector;
+    pair<string, int> element;
+    for (p = Word1.begin(); p != Word1.end(); ++p)
+    {
+        element.first = p->first;
+        element.second = p->second;
+        word_vector.push_back(element);
+    }
+
+    sort(word_vector.begin(), word_vector.end(),
+    word_cmp);
+
+    total = word_vector.size();
+    if (word_vector.size() > 20)
+        num = 20;
+
+    ofstream fout;
+    fout.open(fileName.c_str());
+    //if (fout.is_open())
+        //cout << "文件已打开" << endl;
+    fout << "total "  << word_vector.size() << endl;
+    for (int i = 0; i < num; ++i)
+        fout << word_vector[i].first << ' '
+        << word_vector[i].second
+        << endl;
+    fout.close();
+
+    //排序， 输出频率最高的前20个
+/**
     sort(Word,Word+num,cmp);
     //printf("2");
     freopen(out,"w",stdout);
@@ -206,6 +220,8 @@ void fcounter()
     {
         printf("%s %d\n",Word[i].W,Word[i].cnt);
     }
+*/
+
 }
 int main(int argc, char *argv[])
 {
